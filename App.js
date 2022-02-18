b353a6836aa0f8984ba6fbc0eb1e01b51c91da31
GHTeodor <Theodor.Lviv@gmail.com>
2022-02-18 15:00:59 +0200

    Letters printer

--- App.js
+++ App.js
@@ -12,9 +12,7 @@ app.set('view engine', '.hbs');
 app.engine('.hbs', engine({defaultLayout: false}));
 app.set('views', path.join(__dirname, 'static'));
 
-app.listen(5200, () => {
-    console.log('Server has been started on PORT 5200...');
-});
+app.listen(5200, () => console.log('Server has been started on PORT 5200...'));
 
 const users = [
     {login: 'A', age: 20, password: 'city1'},
@@ -22,13 +20,9 @@ const users = [
     {login: 'C', age: 24, password: 'city3'}
 ];
 
-app.get('/', (req, res) => {
-    res.json(users);
-});
+app.get('/', (req, res) => res.json(users));
 
-app.get('/users', (req, res) => {
-    res.render('users', {users});
-});
+app.get('/users', (req, res) => res.render('users', {users}));
 
 app.get('/users/:userID', (req, res) => {
     const {userID} = req.params;
@@ -36,15 +30,11 @@ app.get('/users/:userID', (req, res) => {
     res.json(users[userID - 1]);
 });
 
-app.get('/login', ((req, res) => {
-    res.render('Login');
-}));
+app.get('/login', ((req, res) => res.render('Login')));
 
 app.post('/login', ((req, res) => {
     users.push(req.body);
     res.redirect('/users');
 }));
 
-app.use((req, res) => {
-    res.render('NotFound');
-});
+app.use((req, res) => res.render('NotFound'));
