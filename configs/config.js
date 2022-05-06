4a379e7ce3ad70e27287f2605ded1fdf8c690ff2
GHTeodor <Theodor.Lviv@gmail.com>
2022-05-07 00:10:25 +0300

    nodemailer,email-templates,.pug

--- configs/config.js
+++ configs/config.js
@@ -5,5 +5,8 @@ module.exports = {
     MONGO_CONNECT_URL: process.env.MONGO_CONNECT_URL || 'mongodb://localhost:27017/june-2021',
 
     JWT_ACCESS_SECRET: process.env.JWT_ACCESS_SECRET,
-    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET
+    JWT_REFRESH_SECRET: process.env.JWT_REFRESH_SECRET,
+    
+    NO_REPLY_EMAIL: process.env.NO_REPLY_EMAIL,
+    NO_REPLY_PASSWORD: process.env.NO_REPLY_PASSWORD
 };
